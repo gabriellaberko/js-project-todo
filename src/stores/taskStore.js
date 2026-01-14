7720e0f5ea04f4d75aaa4a1c4a5617b18d0df01c
Gabriella Berkowicz <gabriellaberkowicz@MacBook-Air-som-tillhor-Gabriella.local>
2026-01-14 10:12:54 +0100

    add persist for saving the tasks in local storage

--- src/stores/taskStore.js
+++ src/stores/taskStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware"; //to save tasks in localStorage
 
-export const useTaskStore = create((set) => ({
+export const useTaskStore = create(persist((set) => ({
   tasks: [
     {
       name: "Task 1",
@@ -30,5 +31,10 @@ export const useTaskStore = create((set) => ({
       ? {...task, completed: !task.completed} //update only the status value for the task which id matched the id sent as parameter
       : task
     )
-   }))
-}))
+  })),
+
+}),
+  {
+    name: 'stored-tasks' //name of the key in localStorage
+  }
+))
